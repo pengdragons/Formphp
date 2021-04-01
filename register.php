d4bc1977e875f2c7c906c6aa574f584f5bb16b17
TrungHoan <nthoan2810@gmail.com>
2021-04-01 15:01:30 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,86 @@
+<?php
+    session_start();
+    $db = mysqli_connect("localhost","root","","testphp");
+    if(isset($_POST['register_btn'])){
+        $username = mysqli_real_escape_string($db,$_POST['username']);
+        $email = mysqli_real_escape_string($db,$_POST['email']);
+        $password = mysqli_real_escape_string($db,$_POST['password']);
+        $confirm_pass = mysqli_real_escape_string($db,$_POST['confirm-password']);
+
+        if($password == $confirm_pass){
+            $password = md5($password);
+            $sql = "INSERT INTO users(username,email,password) VALUES('$username','$email','$password')";
+            mysqli_query($db,$sql);
+            $_SESSION['message'] = "Ok!";
+            $_SESSION['username'] = username;
+            header("location: login.php");
+        }
+        else{
+        }
+    }
+?>
+
+<<!--
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Creative Colorlib SignUp Form</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
+</head>
+<body>
+	<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>Creative SignUp Form</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				<form action="register.php" method="POST">
+					<input class="text" type="text" name="username" placeholder="Username" required="">
+					<input class="text email" type="email" name="email" placeholder="Email" required="">
+					<input class="text" type="password" name="password" placeholder="Password" required="">
+					<input class="text w3lpass" type="password" name="confirm-password" placeholder="Confirm Password" required="">
+					<div class="wthree-text">
+						<label class="anim">
+							<input type="checkbox" class="checkbox" required="">
+							<span>I Agree To The Terms & Conditions</span>
+						</label>
+						<div class="clear"> </div>
+					</div>
+					<input type="submit" name="register_btn" value="SIGNUP">
+				</form>
+				<p>Don't have an Account? <a href="/testphp/login.php"> Login Now!</a></p>
+			</div>
+		</div>
+		<!-- copyright -->
+		<div class="colorlibcopy-agile">
+			<p>© 2018 Colorlib Signup Form. All rights reserved | Design by <a href="https://colorlib.com/" target="_blank">Colorlib</a></p>
+		</div>
+		<!-- //copyright -->
+		<ul class="colorlib-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<!-- //main -->
+</body>
+</html>
